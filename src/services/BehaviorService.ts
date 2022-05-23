08574abd5b7e8016edb4511e7305a3a89744a54f
Sergey Okhotnikov <ognerezov@gmail.com>
2022-05-23 19:23:10 +0200

    fix warnings

--- src/services/BehaviorService.ts
+++ src/services/BehaviorService.ts
@@ -8,37 +8,37 @@ const QUEUE_URL = 'https://sqs.eu-west-1.amazonaws.com/992648939501/landing-even
 const PROJECT = 'Team hrv'
 
 export function report(event : string, ip : string, info : ClientInfo){
-    // const params = {
-    //     MessageAttributes: {
-    //         "Ip": {
-    //             DataType: "String",
-    //             StringValue: ip
-    //         },
-    //         "Event": {
-    //             DataType: "String",
-    //             StringValue: event
-    //         },
-    //         "Language": {
-    //             DataType: "String",
-    //             StringValue: info.language
-    //         },
-    //         "Agent": {
-    //             DataType: "String",
-    //             StringValue: info.userAgent
-    //         },
-    //         "Project": {
-    //             DataType: "String",
-    //             StringValue: PROJECT
-    //         },
-    //     },
-    //     MessageBody: event,
-    //     QueueUrl: QUEUE_URL
-    // };
-    // sqs.sendMessage(params, function(err, data) {
-    //     if (err) {
-    //         console.log("Error", err);
-    //     } else {
-    //         console.log("Success", data.MessageId);
-    //     }
-    // });
+    const params = {
+        MessageAttributes: {
+            "Ip": {
+                DataType: "String",
+                StringValue: ip
+            },
+            "Event": {
+                DataType: "String",
+                StringValue: event
+            },
+            "Language": {
+                DataType: "String",
+                StringValue: info.language
+            },
+            "Agent": {
+                DataType: "String",
+                StringValue: info.userAgent
+            },
+            "Project": {
+                DataType: "String",
+                StringValue: PROJECT
+            },
+        },
+        MessageBody: event,
+        QueueUrl: QUEUE_URL
+    };
+    sqs.sendMessage(params, function(err, data) {
+        if (err) {
+            console.log("Error", err);
+        } else {
+            console.log("Success", data.MessageId);
+        }
+    });
 }
