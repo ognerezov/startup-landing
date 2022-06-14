22e754dec532a1aee3fe58620ce2810e498eecf4
Sergey Okhotnikov <ognerezov@gmail.com>
2022-06-14 23:16:40 +0200

    +try

--- src/components/maps/MapBoxConfig.js
+++ src/components/maps/MapBoxConfig.js
@@ -1,4 +1,6 @@
 import mapboxgl from 'mapbox-gl';
+import MapboxWorker from 'mapbox-gl/dist/mapbox-gl-csp-worker';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoicG9sYXJvc28iLCJhIjoiY2w0NWx0OHA3MDI3bTNrbjZyeWIxcG95aSJ9.XJVCOv41BrAzsKm9Ye2ygQ';
-mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default; // eslint-disable-line
+mapboxgl.workerClass = MapboxWorker
+// mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default; // eslint-disable-line
